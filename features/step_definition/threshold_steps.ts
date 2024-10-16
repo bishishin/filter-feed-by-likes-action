be36ed531e016288e2122d8b7b043769d3b0f05f
bishishin <dummy@>
2024-10-16 09:44:05 +0900

    fix lint

--- features/step_definition/threshold_steps.ts
+++ features/step_definition/threshold_steps.ts
@@ -2,45 +2,45 @@ import { Given, Then, When, World } from "@cucumber/cucumber";
 import { expect } from "chai";
 import type * as gen from "feed";
 import {
-	type ActionEnvironment,
-	type ActionInputs,
-	FeedGenerator,
+  type ActionEnvironment,
+  type ActionInputs,
+  FeedGenerator,
 } from "../../src/generate";
 
 class CustomWorld extends World {
-	threshold: number;
-	items: gen.Item[];
-	scores: Map<string, number>;
+  threshold: number;
+  items: gen.Item[];
+  scores: Map<string, number>;
 }
 
 class FeedGenForTest extends FeedGenerator {
-	publishItemsOnHighScoreForTest = this.publishItemsOnHighScore;
+  publishItemsOnHighScoreForTest = this.publishItemsOnHighScore;
 }
 
 Given("配信閾値設定が10", function (this: CustomWorld) {
-	this.threshold = 10;
+  this.threshold = 10;
 });
 
 When("記事の評価が{int}", function (this: CustomWorld, score: number) {
-	this.scores = new Map([["hoge", score]]);
-	this.items = [{ title: "", link: "hoge", date: new Date() }];
+  this.scores = new Map([["hoge", score]]);
+  this.items = [{ title: "", link: "hoge", date: new Date() }];
 });
 
 Then(
-	"対象記事を生成するフィードに配信{word}",
-	function (this: CustomWorld, publishable: string) {
-		const generator = new FeedGenForTest(
-			{} as ActionEnvironment,
-			{ threshold: this.threshold } as ActionInputs,
-		);
-		const items = generator.publishItemsOnHighScoreForTest(
-			this.items,
-			this.scores,
-		);
-		if (publishable === "可能") {
-			expect(items).to.be.not.empty;
-		} else {
-			expect(items).to.be.empty;
-		}
-	},
+  "対象記事を生成するフィードに配信{word}",
+  function (this: CustomWorld, publishable: string) {
+    const generator = new FeedGenForTest(
+      {} as ActionEnvironment,
+      { threshold: this.threshold } as ActionInputs,
+    );
+    const items = generator.publishItemsOnHighScoreForTest(
+      this.items,
+      this.scores,
+    );
+    if (publishable === "可能") {
+      expect(items).to.be.not.empty;
+    } else {
+      expect(items).to.be.empty;
+    }
+  },
 );
